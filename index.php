0ba9cc87133a97ab7bdac5d5058ebc5bfe4c1a75
princepalsingh <prince00736@gmail.com>
2019-04-10 15:49:23 +0530

    Improved File Structure

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="fileupload.css">
-        <link rel="stylesheet" href="multipleImageUpload.css">
+        <link rel="stylesheet" href="assets/css/imageUpload.css">
+        <link rel="stylesheet" href="assets/css/multipleImageUpload.css">
         <title>Hello, world!</title>
     </head>
     <body>
@@ -58,8 +58,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-        <script src="imageUpload.js"></script>
-        <script src="multipleImageUpload.js"></script>
+        <script src="assets/js/imageUpload.js"></script>
+        <script src="assets/js/imageUploadExtension.js"></script>
         <script>
               var app = new Vue({
                 el: '#app',
